0097584ff19743a6758d7a0bfd8f0127c2bc6828
gminova <minova1995@gmail.com>
2019-07-30 15:46:31 +0100

    rfactored server

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('Hello FAC17\n');
 });
 
 server.listen(port, hostname, () => {
