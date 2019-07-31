615f852fa50af0556f2a96a6c811452f6473f7a3
Gergana Minova <42014388+gminova@users.noreply.github.com>
2019-07-31 11:20:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello FAC17\n");
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at port: ${port}`);
 });
