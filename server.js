90f4e71fee9333196ff87c95b15dd5098f3c19f3
Gergana Minova <42014388+gminova@users.noreply.github.com>
2019-07-31 09:16:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = process.env.HOSTNAME || '127.0.0.1';
+const hostname = process.env.HOSTNAME || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello FAC17\n');
+  res.writeHead(200, {"Content-Type":"text/plain"});
+  res.end("Hello FAC17\n");
 });
 
 server.listen(port, hostname, () => {
